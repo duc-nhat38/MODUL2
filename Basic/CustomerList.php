64ff0a4bc364d23a2986bb9f4808662659c8537c
duc-nhat38 <phamdinhduc02071998@gmail.com>
2020-06-07 09:39:26 +0700

    ùe

--- Basic/CustomerList.php
+++ Basic/CustomerList.php
@@ -6,68 +6,92 @@ $customer_list = array(
     "3" => array("name" => "Trần Đăng Khoa", "day_of_birth" => "1983/08/17", "address" => "Hà Tây"),
     "4" => array("name" => "Nguyễn Đình Thi", "day_of_birth" => "1983/08/19", "address" => "Hà Nội")
 );
-$from_date = $_POST['date1'];
-$to_date = $_POST['date2'];
-function CustomerList($customers,$date1, $date2) {
-    if (empty($date1) && empty($date2)) {
+function searchByDate($customers, $from_date, $to_date)
+{
+    if (empty($from_date) && empty($to_date)) {
         return $customers;
     }
     $arr = [];
     foreach ($customers as $customer) {
-        if (strtotime($customer["day_of_birth"]) > strtotime($date1) && strtotime($customer["day_of_birth"]) < strtotime($date2)) {
-            $arr[$customer];
+        if (!empty($from_date) && strtotime($customer['day_of_birth']) < strtotime($from_date)) {
+            continue;
         }
+        if (!empty($to_date) && strtotime($customer['day_of_birth']) > strtotime($to_date)) {
+            continue;
+        }
+        return $arr[] = $customer;
     }
-    return $arr;
 }
+$from_date = NULL;
+$to_date = NULL;
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $from_date = $_POST['date1'];
+    $to_date = $_POST['date2'];
+}
+$filtered_customers = searchByDate($customer_list, $from_date, $from_date);
 
-$ar = CustomerList($customer_list, $from_date,  $to_date);
-echo ('<table>');
-    echo ('<tr>');
-    echo ('<th>' . 'Stt' . '</th>');
-    echo ('<th>' . 'Tên' . '</th>');
-    echo ('<th>' . 'Ngày sinh' . '</th>');
-    echo ('<th>' . 'Địa chỉ' . '</th>');
-    echo ('</tr>');
-foreach ($ar as $key => $val) {
-    echo ('<tr>');
-        echo ('<td>' . $key . '</td>');
-        echo ('<td>' . $val['name'] . '</td>');
-        echo ('<td>' . $val['day_of_birth'] . '</td>');
-        echo ('<td>' . $val['address'] . '</td>');
-        echo ('</tr>');
-    };
-    echo ('</table>');
 
 ?>
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Custtomer List</title>
 </head>
+
 <body>
     <form action="CustomerList.php" method="post">
         <table>
 
-        <tr>
-            <td><p>From:</p></td>
-            <td>
-                <label for="date"></label>
-                <input type="date" name="date1" id="date">
-            </td>
-            <td><p>To:</p></td>
-            <td>
-            <label for="date"></label>
-                <input type="date" name="date2" id="date">
-            </td>
-            <td>
-                <input type="submit">
-            </td>
-        </tr>
+            <tr>
+                <td>
+                    <p>From:</p>
+                </td>
+                <td>
+                    <label for="date"></label>
+                    <input type="text" name="date1" id="date" placeholder="yyyyy/mm/dd">
+                </td>
+                <td>
+                    <p>To:</p>
+                </td>
+                <td>
+                    <label for="date"></label>
+                    <input type="text" name="date2" id="date" placeholder="yyyyy/mm/dd">
+                </td>
+                <td>
+                    <input type="submit">
+                </td>
+            </tr>
         </table>
     </form>
-    
+    <table>
+        <caption>
+            <h2>Danh sách khách hàng</h2>
+        </caption>
+        <tr>
+            <th>STT</th>
+            <th>Tên</th>
+            <th>Ngày sinh</th>
+            <th>Địa chỉ</th>
+        </tr>
+         <?php
+         if(count($filtered_customers) === 0) {
+             echo "<tr>";
+             echo "<td>"."Không tìm thấy khách hàng nào."."</td>";
+         }
+         foreach ($filtered_customers as $key => $value) {
+             echo "<tr>";
+             echo "<td>".$key."<td>";
+             echo "<td>".$value['name']."<td>";
+             echo "<td>".$value['day_of_birth']."<td>";
+             echo "<td>".$value['address']."<td>";
+             echo "</tr>";
+         }
+         ?>
+        
+    </table>
 </body>
+
 </html>
---
+++ Basic/layout/layout.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <style>
+        <?php include "css.php"; ?>
+    </style>
+</head>
+
+<body>
+    <div class="container">
+        <header> <?php include "header.php"; ?> </header>
+        <nav> <?php include "nav.php"; ?> </nav>
+        <article> <?php include "content.php"; ?></article>
+        <footer><?php include "footer.php"; ?></footer>
+    </div>
+</body>
+
+</html>
