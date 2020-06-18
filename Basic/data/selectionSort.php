85ef70518e52f713d9470cb1d41fee8005a805d5
duc-nhat38 <phamdinhduc02071998@gmail.com>
2020-06-18 14:40:11 +0700

    b

--- Basic/data/selectionSort.php
+++ Basic/data/selectionSort.php
@@ -1,16 +1,21 @@
 <?php
- function selectionSort($list){
-     for($i = 0;$i<count($list);$i++){
-         $value = $list[$i];
-         $j = $i-1;
-         while($j>=0 && $list[$j]>$value){
-            $list[$j+1] = $list[$j];
-            $j--;
-         }
-         $list[$j+1] = $value;
-     }
-     return $list;
- }
-
- $list = [9,2,56,10,2,35,7];
- print_r(selectionSort($list));
+function selectionSort($list)
+{
+    for ($i = 0; $i < count($list); $i++) {
+        $min = $i;
+        $tmp = 0;
+        for ($j = $i + 1; $j < count($list); $j++) {
+            if ($list[$min] > $list[$j]) {
+                $min = $j;
+            }
+        }
+        if ($min !== $i) {
+            $tmp = $list[$i];
+            $list[$i] = $list[$min];
+            $list[$min]=$tmp;
+        }
+    }
+    return $list;
+}
+$list = [9, 2, 56, 10, 2, 35, 7];
+print_r(selectionSort($list));
